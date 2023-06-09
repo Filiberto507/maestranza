9133451a36418cbea386b31f6c56f2bb1873228c
Filiberto507 <choque507.es@gmail.com>
2023-06-09 16:07:29 -0400

    primer commit

---
+++ resources/views/layouts/theme/header.blade.php
@@ -0,0 +1,66 @@
+<div class="header-container fixed-top" >
+    <header class="header navbar navbar-expand-sm" style="background: #cac3c1">
+        <ul class="navbar-item flex-row" >
+            <li class="nav-item theme-logo">
+                <a href="{{url('/home')}}">
+                    <img src="assets/img/90x90.jpg" class="navbar-logo" alt="logo">
+                </a>
+            </li>
+        </ul>
+
+        <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg></a>
+
+       
+            
+        <ul class="navbar-item flex-row navbar-dropdown">
+
+            <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
+                <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="assets/img/90x90.jpg" alt="admin-profile" class="img-fluid">
+                </a>
+                <div class="dropdown-menu position-absolute animated fadeInUp" aria-labelledby="userProfileDropdown">
+                    <div class="user-profile-section">
+                        <div class="media mx-auto">
+                            <img src="assets/img/90x90.jpg" class="img-fluid mr-2" alt="avatar">
+                            <div class="media-body">
+                                <!-- integracion de los nombres en el perfil  -->
+                                @if(Auth::user()=='')
+                                    
+                                    <h5>Alan Green</h5>
+                                    <p>Project Leader</p>
+                                 @else 
+                                  <h5> {{Auth::user()->name}} </h5>
+                                  <p>{{Auth::user()->profile}}</p>
+                                
+                                @endif
+                                
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="dropdown-item">
+                        @if(Auth::user()=='')
+                            <a href=""  >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> <span>Mi Perfil</span>
+                            </a>
+                        @else
+                            <a href="{{ url('component' . '/' . Auth::user()->id) }}"  >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> <span>Mi Perfil</span>
+                            </a>
+                        @endif
+                    </div>
+                 
+                    <div class="dropdown-item">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg> <span>Salir</span>
+                        </a>
+                        <form action="{{ route('logout') }}" method="POST" id="logout-form" >
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </header>
+</div>
+
