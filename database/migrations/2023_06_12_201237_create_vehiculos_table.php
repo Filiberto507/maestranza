d6015305e63474f39738a6408a9116a38239c2f9
Filiberto507 <choque507.es@gmail.com>
2023-06-13 12:22:32 -0400

    migraciones de las diferentes tablas

---
+++ database/migrations/2023_06_12_201237_create_vehiculos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->char('placa',10);
+            $table->string('tipo',45);
+            $table->string('nombre',45);
+            $table->string('marca',45);
+            $table->string('modelo',45);
+            $table->string('color',45);
+            $table->string('cilindrada',45);
+            $table->string('chasis',45);
+            $table->string('motor',45);
+            $table->unsignedBigInteger('dependencias_id');
+ 
+            $table->foreign('dependencias_id')->references('id')->on('dependencias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+};
---
+++ database/migrations/2023_06_12_204147_create_diagnosticos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diagnosticos', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('fecha');
+            $table->integer('item');
+            $table->string('descripcion',250);
+            $table->text('observaciones',450);
+            $table->unsignedBigInteger('vehiculos_id');
+ 
+            $table->foreign('vehiculos_id')->references('id')->on('vehiculos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diagnosticos');
+    }
+};
